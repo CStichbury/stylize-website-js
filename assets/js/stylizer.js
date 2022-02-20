bb749a03ba5db75d1bce539e229f2d6daa686a52
tornikeo <tornikeonoprishvili@gmail.com>
2022-02-20 13:59:11 +0400

    replaced bs3 to bs5, added image saving

--- assets/js/stylizer.js
+++ assets/js/stylizer.js
@@ -15,6 +15,9 @@ function isFileImage(file) {
   return file && $.inArray(file["type"], acceptedImageTypes);
 }
 
+let content_img = null;
+let style_img = null;
+
 $(function () {
   $(document).on("change", ".uploadFile", function () {
     var uploadFile = $(this);
@@ -25,7 +28,6 @@ $(function () {
       var reader = new FileReader(); // instance of the FileReader
       reader.readAsDataURL(files[0]); // read the local file
       console.log('Hello');
-
       reader.onloadend = function () {
         // set image data as background of div
         //alert(uploadFile.closest(".upimage").find('.imagePreview').length);
@@ -33,6 +35,19 @@ $(function () {
           .closest(".imgUp")
           .find(".imagePreview")
           .css("background-image", "url(" + this.result + ")");
+          
+        // Save the image
+        const input_name = $(this).attr('id');
+        if(input_name.contains('content')){
+          content_img = this.result;
+        } else if (input_name.contains('style')){
+          style_img = this.result;
+        };
+
+        // If images are loaded, unlock the stylize button
+        if(content_img && style_img){
+          $('#Stylize').removeAttr('disabled');
+        }
       };
     }
   });
@@ -53,12 +68,25 @@ const delay = millis => new Promise((resolve, reject) => {
   setTimeout(_ => resolve(), millis)
 });
 
+const loading_state = `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>Stylizing...`
+
 $('#Stylize').click(async function(){
-  console.log('YAY!');
-  $('#Stylize').attr('disabled', 'disabled');
+  const prev_state = $('#Stylize').html()
+
+  $('#Stylize').toggleClass('disabled');
   
+  $('#Stylize').empty().append(loading_state);
+
   await delay(1000);
-	// if ($('#Stylize').is(':disabled')) {
-  $('#Stylize').removeAttr('disabled');
-  // }
+  
+  $('#Stylize').toggleClass('disabled');
+
+  $('#Stylize').empty().append(prev_state);
 });
+
+// From this tutorial: https://getbootstrap.com/docs/5.0/components/tooltips/
+// Need to add popper js properly
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
